31587e79f15d64ed132802c25218795274a8900e
UnamataSanatarai <unamatasanatarai@gmail.com>
2019-09-06 13:44:08 +0200

    cleanup and templating

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-from app.http import routes
+from app import routes
--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,9 +1,8 @@
-from app.http import app
-from flask import Flask, Response, request
+from app import app
+from flask import Flask, Response, request, render_template
 from html import escape
 from json import dumps
 from os.path import dirname
-from app.view import render
 
 def getLinks():
     return [
@@ -13,11 +12,9 @@ def getLinks():
         "/text/YOUR_TEXT/and?then=YOUR_QUERY_TEXT",
     ]
 
-views = dirname(dirname(__file__))+'/resources/views'
-
 @app.route('/')
 def index():
-    return render('index.html', directory=views, links=getLinks())
+    return render_template('home/index.html', links=getLinks())
 
 @app.route('/json')
 def json():
@@ -30,16 +27,16 @@ def json():
 
 @app.route('/text')
 def text():
-    return "pure simple text"
+    return render_template('home/text.html')
 
 @app.route('/text/<input>')
 def textinput(input = None):
     input = escape(input)
-    return f'You said: {input}'
+    return render_template('home/textinput.html', input=input)
 
 
 @app.route('/text/<input>/and')
 def textinputand(input = None):
     input = escape(input)
     q = escape(request.args.get('then', default="nothing"))
-    return f'You said: {input} and {q}'
+    return render_template('home/textinputand.html', input=input, then=q)
